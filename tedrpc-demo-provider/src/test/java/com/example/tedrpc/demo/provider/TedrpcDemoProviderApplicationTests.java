ccb4faee9959ff8f9813437ec27aced8ef8fcfb4
linkuan <feibu2728zhidao@163.com>
2024-03-29 01:05:44 +0800

    update provider pom to fix mvn test

--- tedrpc-demo-provider/src/test/java/com/example/tedrpc/demo/provider/TedrpcDemoProviderApplicationTests.java
+++ tedrpc-demo-provider/src/test/java/com/example/tedrpc/demo/provider/TedrpcDemoProviderApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.tedrpc.demo.provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(classes = TedrpcDemoProviderApplication.class)
 class TedrpcDemoProviderApplicationTests {
 
 	@Test
