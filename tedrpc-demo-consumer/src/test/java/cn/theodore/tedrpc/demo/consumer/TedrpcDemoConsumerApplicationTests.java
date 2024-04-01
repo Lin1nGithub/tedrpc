b438f8b50716a82deb05bb8b6f982abe56a165d5
linkuan <feibu2728zhidao@163.com>
2024-04-01 22:11:01 +0800

    update consumer application test

--- tedrpc-demo-consumer/src/test/java/cn/theodore/tedrpc/demo/consumer/TedrpcDemoConsumerApplicationTests.java
+++ tedrpc-demo-consumer/src/test/java/cn/theodore/tedrpc/demo/consumer/TedrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,36 @@
 package cn.theodore.tedrpc.demo.consumer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+/**
+ * consumer的单测依赖provider
+ */
 @SpringBootTest
+@Slf4j
 class TedrpcDemoConsumerApplicationTests {
+
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void init() {
+        context = SpringApplication.run(TedrpcDemoConsumerApplication.class,"--server.port=8084");
+    }
 
     @Test
     void contextLoads() {
+        log.info("=====> aaaaaaaaaaaaaa");
     }
+
+    @AfterAll
+    static void destroy(){
+        SpringApplication.exit(context, () -> 1);
+    }
+
 
 }
